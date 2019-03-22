c0a8ef19dae84eac184a87921295f51c57a8cdfe
Francesco Benfenati <francesco.benfenati@gmail.com>
2019-03-22 18:49:47 +0100

    rule sum_all

---
+++ sum_all.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 22 18:29:24 2019
+
+@author: francescobenfenati
+"""
+
+from transactions import sum_transactions
+sum_transactions("transazioni_corrette.tsv","finale.tsv")
