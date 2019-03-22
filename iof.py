104b3ead4b7f7df116369a8fd6c4d0818e3f550a
Francesco Benfenati <francesco.benfenati@gmail.com>
2019-03-22 16:48:21 +0100

    made functions

--- iof.py
+++ iof.py
@@ -1,4 +1,4 @@
-
+import os
 def check_for_equals(a):                               #restituisce liste degli indici di membri uguali
     coppie = []    
 
@@ -32,50 +32,48 @@ def check_for_equals(a):                               #restituisce liste degli 
         
     return coppie
 
-
-f=open("test.txt",'r')
-t=open("test.txt",'r')
-l=open("test.txt",'r')
-w = open("merged",'w')
-nomi = []
-money = []
-line_counter = 0
-for line in l:
-    line_counter +=1
-    
-for x in range(line_counter):
-    size = len(f.readline())
-    a = []
-    for i in range(size):
-        a.append(t.read(1))
-    
-        if a[i] == "\t":
-            b = t.read(1)
-            leggo = t.read(1)
-            if leggo is "":
-                print("end of file")
+def sum_transactions(filename,out):
+    f=open(filename,'r')
+    w = open(out,'w')
+    names = []
+    money = []
+    line_counter = 0 
+    for line in f:
+        line_counter +=1
+    f.seek(0)
+    for x in range(line_counter): #leggo ogni riga
+        line = f.readline()
+        line_size = len(line)
+        a = []
+        f.seek(f.tell()-line_size, os.SEEK_SET) 
+        for i in range(line_size):              #leggo byte per byte
+            a.append(f.read(1))
+            if a[i] == "\t":                    #dopo il tab leggo i soldi
+                b = f.read(1)
+                leggo = f.read(1)
+                if leggo is "":
+                    #print("end of file")
+                    break
+                while leggo != "\n":
+                    b+=leggo
+                    leggo = f.read(1)
                 break
-            while leggo != "\n":
-                b+=leggo
-                leggo = t.read(1)
-            break
-    nome = ''.join(a[0:-1])
-    nomi.append(nome)
-    b = int(b)
-    money.append(b)
-f.close()
-t.close()
-print(nomi,money)
-equals = check_for_equals(nomi)
-print(equals)
-for i in range(len(equals)):
-    w.write(nomi[equals[i][0]])
-    w.write("\t")
-    tot = 0
-    for j in range(len(equals[i])):
-        tot += money[equals[i][j]]
-    w.write(str(tot))
-    w.write("\n")
-w.close()
+        name = ''.join(a[0:-1])
+        names.append(name)
+        b = int(b)
+        money.append(b)
+    f.close()
+    equals = check_for_equals(names)
+    for i in range(len(equals)):                #scrivo i nomi una sola volta
+        w.write(names[equals[i][0]])
+        w.write("\t")
+        tot = 0
+        for j in range(len(equals[i])):
+            tot += money[equals[i][j]]          #con i soldi totali
+        w.write(str(tot))
+        w.write("\n")
+    w.close()
+    return
 
+sum_transactions("test.txt","merged.txt")
 
